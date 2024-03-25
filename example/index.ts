b673952bee61af884133cc38d0b3622a4f27aad8
John Stassi <jbstassi@gmail.com>
2024-03-25 13:38:55 -0500

    Update index.ts
    Added line to generate FHIR R4 declarative typescript file based on fhir.R4.schema.json file

--- example/index.ts
+++ example/index.ts
@@ -3,6 +3,7 @@ import {compileFromFile} from 'json-schema-to-typescript'
 
 async function generate() {
   writeFileSync('person.d.ts', await compileFromFile('person.json'))
+  writeFileSync('fhir.R4.schema.d.ts', await compileFromFile('fhir.R4.schema.json'))
 }
 
 generate()
